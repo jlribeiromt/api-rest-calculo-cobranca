ce49590b0a18f08bf08263585feedcd5bf845fc6
jlribeiromt <jl.dr@hotmail.com>
2021-01-24 16:36:27 -0400

    Implementação do projeto de teste unitário de integração com a Api de Cálculo de Juros, com a coberturas dos endponts de "/calculajuros" e "/showmethecode".

---
+++ CalculoCobranca.CalculaJuro.Api.IntegrationTest/Provider/ClientProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+
+namespace CalculoCobranca.CalculaJuro.Api.IntegrationTest.Provider
+{
+    public class ClientProvider
+    {
+        protected readonly HttpClient TestClient;
+
+        public ClientProvider()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }      
+    }
+}
