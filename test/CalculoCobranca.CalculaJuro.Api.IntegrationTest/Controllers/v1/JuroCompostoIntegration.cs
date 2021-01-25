6fb0c22e86cf47fb47549b3e6b80c2082a73b2a6
jlribeiromt <jl.dr@hotmail.com>
2021-01-24 21:45:43 -0400

    Inclusão do projeto de integração de api.

---
+++ test/CalculoCobranca.CalculaJuro.Api.IntegrationTest/Controllers/v1/JuroCompostoIntegration.cs
@@ -0,0 +1,20 @@
+﻿using CalculoCobranca.CalculaJuro.Api.IntegrationTest.Provider;
+using FluentAssertions;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculoCobranca.CalculaJuro.Api.IntegrationTest.Controllers.v1
+{
+    public class JuroCompostoIntegration : ClientProvider
+    {
+        [Fact]
+        public async Task Quando_executar_com_sucesso_Calculo_Juro_ReturnOK()
+        {            
+            var response = await TestClient.GetAsync(requestUri: "/api/v1/taxa/taxaJuros?valorInicial=100&meses=5");
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ test/CalculoCobranca.CalculaJuro.Api.IntegrationTest/Provider/ClientProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace CalculoCobranca.CalculaJuro.Api.IntegrationTest.Provider
+{
+    public class ClientProvider
+    {
+        protected readonly HttpClient TestClient;
+
+        public ClientProvider()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }      
+    }
+}
